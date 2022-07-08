cc3a05f705d2f2ed72bedbf7e2f205d84c7553fd
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-07-08 08:53:30 -0600

    Update categorias.service.ts

--- Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Services/categorias.service.ts
+++ Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Services/categorias.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriasService {
 
-  constructor() { }
+  constructor(private http:HttpClient, private router:Router) { }
+  crearCategoria(objCategoria:any): Observable<any> {
+    let urlStr = `http://localhost:3000/categoria/registro/`
+      return this.http.post(urlStr, objCategoria)
+  }
+
+  getCategorias():Observable<any>{
+    let urlC = 'http://localhost:3000/categorias/';
+    return this.http.get(urlC)
+
+  }
 }
