2e361418698e4a4b24746cd8383ac0d7c5a79936
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-07-08 08:53:01 -0600

    Update productos.service.ts

--- Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Services/productos.service.ts
+++ Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Services/productos.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  crearProducto(objUsuario:any): Observable<any> {
+    let urlStr = `http://localhost:3000/producto/registro/`
+      return this.http.post(urlStr, objUsuario)
+  }
+  getProductos(): Observable<any>{
+    let url = 'http://localhost:3000/producto/';
+    return this.http.get(url);
+  }
 }
