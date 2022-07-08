360a5321b2381953f4730c1eab8236c2c358cf57
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-07-08 08:30:00 -0600

    Update header.component.ts

--- Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/landing/header/header.component.ts
+++ Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/landing/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import leerToken from 'src/app/helpers/decodificarToken'
+import leerToken from 'src/app/helpers/decodificarToken';
+import {AuthService} from 'src/app/auth/auth.service';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/Services/usuario.service';
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,13 +15,43 @@ export class HeaderComponent implements OnInit {
   @Input() urlItemActual:string = "/";
 
 
-
+  usuarioActual:any;
   token:any = leerToken();
-  constructor() { }
+  
+  constructor(private authService: AuthService,private router: Router, private usuarioService: UsuarioService) { }
+
+  cerrar(){
+    this.authService.cerrarSesion();
+    this.router.navigateByUrl('/inicio')
+  }
 
   ngOnInit(): void {
+    console.log(this.token);
 
-
+    if(this.token?.correo){
+      this.usuarioService.getUsuario(this.token.correo).subscribe((res:any)=>{
+        this.usuarioActual=res;
+        console.log(res)
+      })
+    }
   }
+
+  comprobarUsuarioAdmin(){
+    if(this.usuarioActual && this.usuarioActual.idRol==3) {
+      return true
+    }else{
+      return false
+    }
+  }
+
+  comprobarUsuario(){
+    if(this.usuarioActual) {
+      return true
+    }else{
+      return false
+    }
+  }
+
+
 
 }
