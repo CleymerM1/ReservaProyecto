f8638c87736f1c67e5b870313356ef641eedcb97
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-07-08 08:11:06 -0600

    Update BD_ACTUALIZADA.sql

--- Reserva Proyecto/Backend/BaseDatos/BD_ACTUALIZADA.sql
+++ Reserva Proyecto/Backend/BaseDatos/BD_ACTUALIZADA.sql
@@ -1,9 +1,4 @@
-drop user 'admin1'@'localhost';
 
-create user 'admin1'@'localhost' identified with mysql_native_password by 'B@rr@ntes_624';
-grant all privileges on  *.* to 'admin1'@'localhost';
-flush privileges;
-drop database Pagina_web;
 
 CREATE DATABASE Pagina_Web;
 USE Pagina_Web;
@@ -12,10 +7,13 @@ CREATE TABLE privilegios (idPrivilegio INT PRIMARY KEY AUTO_INCREMENT, visualiza
 
 CREATE TABLE categoria (idCategoria INT PRIMARY KEY AUTO_INCREMENT, nombreCategoria VARCHAR(50), descripcion VARCHAR(50));
 DROP TABLE producto;
-CREATE TABLE producto(idProducto INT auto_increment,idCategoria INT, nombre VARCHAR(50), tipo VARCHAR(50), costo INT, foto varchar(50),
-estado VARCHAR(50), descripcion VARCHAR(50), descuento INT,
+
+CREATE TABLE producto(idProducto INT auto_increment,idCategoria INT, nombre VARCHAR(50), costo INT,
+estado VARCHAR(50), descripcion VARCHAR(50),
 primary key (idProducto, idCategoria),
 FOREIGN KEY (idCategoria) REFERENCES categoria(idCategoria));
+
+CREATE TABLE foto(idFoto int primary key auto_increment, idProducto int, descripcion varchar(150), foreign key (idProducto) references producto(idProducto));
 
 CREATE TABLE rol(idRol INT AUTO_INCREMENT, idPrivilegio INT,nombreRol VARCHAR(50), estado VARCHAR(50),
 primary key (idRol),
@@ -41,3 +39,5 @@ ALTER TABLE usuario MODIFY contrasenia blob;
 ALTER TABLE usuario ADD token TEXT;
 ALTER TABLE usuario ADD confirmado ENUM('1','0') DEFAULT '0'
 ALTER TABLE categoria ADD imagen LONGBLOB;
+ALTER TABLE producto ADD ubicacion VARCHAR(50);
+insert into rol (idPrivilegio, nombreRol, estado) values (2,'admin', 'activo');
