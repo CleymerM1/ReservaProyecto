fb11033ca9b7d6253e8b2c9523aa62ab3550f987
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-07-08 08:35:34 -0600

    Update listar-productos.component.ts

--- Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/listar-productos/listar-productos.component.ts
+++ Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/listar-productos/listar-productos.component.ts
@@ -10,9 +10,9 @@ import { Producto } from 'src/app/models/producto';
 export class ListarProductosComponent {
 
   productoArray: Producto[] = [
-    {nombre : "Laptop", categoria: "Electronica", estado: "Nuevo", descripcion: "HP de 32 GB Ram, 286 SSD",descuento: 0.5, ubicacion: "Comayagua", precio: "16000"},
-    {nombre : "Telefono", categoria: "Electronica", estado: "Nuevo", descripcion: "HP de 32 GB Ram, 286 SSD",descuento: 0.5, ubicacion: "Comayagua", precio: "16000"},
-    {nombre : "Televisor", categoria: "Electronica", estado: "Nuevo", descripcion: "HP de 32 GB Ram, 286 SSD",descuento: 0.5, ubicacion: "Comayagua", precio: "16000"}
+    {nombre : "Laptop", categoria: "Electronica", estado: "Nuevo", descripcion: "HP de 32 GB Ram, 286 SSD",ubicacion: "Comayagua", costo: "16000"},
+    {nombre : "Telefono", categoria: "Electronica", estado: "Nuevo", descripcion: "HP de 32 GB Ram, 286 SSD", ubicacion: "Comayagua", costo: "16000"},
+    {nombre : "Televisor", categoria: "Electronica", estado: "Nuevo", descripcion: "HP de 32 GB Ram, 286 SSD", ubicacion: "Comayagua", costo: "16000"}
   
   
   ];
