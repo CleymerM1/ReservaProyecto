e6268ec782b5989f4099ed09ef5acf85dbacd66d
CleymerM1 <79803554+CleymerM1@users.noreply.github.com>
2022-07-09 10:32:00 -0600

    Update categoria.js

--- Reserva Proyecto/Backend/routes/categoria.js
+++ Reserva Proyecto/Backend/routes/categoria.js
@@ -7,6 +7,7 @@ const checkAuth = require('../middleware/checkAuth.js');
 router.post('/registro', CategoriaController.crearCategoria);
 //router.post('/login',categoriaController.inicioSesion);
 router.get('/', CategoriaController.obtenerCategorias);
+router.get('/:id', CategoriaController.obtenerCategoria)
 
 
 
