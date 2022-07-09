6c05aa6a626f51b48aee33d3cf8ba55257b54845
CleymerM1 <79803554+CleymerM1@users.noreply.github.com>
2022-07-09 10:15:19 -0600

    Update categoria.component.ts

--- Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/categoria/categoria.component.ts
+++ Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/categoria/categoria.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Categoria } from 'src/app/interfaces/Categorias';
+import { CategoriasService } from 'src/app/Services/categorias.service';
 @Component({
   selector: 'app-categoria',
   templateUrl: './categoria.component.html',
@@ -18,11 +19,14 @@ export class CategoriaComponent implements OnInit {
 
   @Input() categoriaActual:Categoria | undefined;
 
-  constructor( private route: ActivatedRoute) { }
+  constructor( private route: ActivatedRoute, private categoriaService:CategoriasService) { }
+
+  mostarFormulario = false;
 
   ngOnInit(): void {
     let idCategoria = parseInt(this.route.snapshot.paramMap.get('idCategoria') || "")
-    this.categoriaActual = this.categorias.filter(cat => cat.idCategoria == idCategoria)[0]
+    //this.categoriaActual = this.categorias.filter(cat => cat.idCategoria == idCategoria)[0]
+    this.obtenerCategoria(idCategoria)
     this.obtenerProductos()
 
   }
@@ -31,6 +35,17 @@ export class CategoriaComponent implements OnInit {
   obtenerProductos() {
     console.log(`Obteniendo los productos para la categoría: `)
     console.log(this.categoriaActual)
+  }
+
+  setMostrarFormulario() {
+    this.mostarFormulario = !this.mostarFormulario;
+  }
+
+  obtenerCategoria(idCategoria:number) {
+    this.categoriaService.getCategoria(idCategoria).subscribe( (res:any) => {
+      this.categoriaActual = res;
+      console.log(res)
+    })
   }
 
 }
