924153e7e70f58118a8137b8de351ade6ecf499d
CleymerM1 <79803554+CleymerM1@users.noreply.github.com>
2022-07-09 10:18:50 -0600

    Add files via upload

---
+++ Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/imagenes-producto/imagenes-producto.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-imagenes-producto',
+  templateUrl: './imagenes-producto.component.html',
+  styleUrls: ['./imagenes-producto.component.css']
+})
+
+
+export class ImagenesProductoComponent implements OnInit {
+
+  @Input() imagenes:any = []
+  @Input() cols:string = "col-12"
+  @Output() onDeleteImage = new EventEmitter<any>()
+
+
+  constructor(  ) { 
+  }
+
+  ngOnInit(): void {
+    console.log(this.imagenes[0])
+  }
+
+  eliminarImagen(name:any) {
+    let imagenesActualizado = this.imagenes.filter( (imagen:any) => imagen.name !== name )
+    this.onDeleteImage.emit(imagenesActualizado)
+  }
+
+
+}
