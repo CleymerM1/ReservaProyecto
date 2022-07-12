4210066bce51646bdee44e9435f30e62d4ed64a0
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-07-12 10:23:07 -0600

    Update producto.component.ts

--- Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/producto/producto.component.ts
+++ Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/producto/producto.component.ts
@@ -1,12 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Categoria } from 'src/app/interfaces/Categorias';
-import { UsuarioService } from 'src/app/Services/usuario.service';
-import { TransitionCheckState } from '@angular/material/checkbox';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Producto } from 'src/app/models/producto';
+import leerToken from 'src/app/helpers/decodificarToken';
+import { Categoria } from 'src/app/interfaces/Categorias';
 import { ProductosService } from 'src/app/Services/productos.service';
-import { NgModel } from '@angular/forms';
+import { UsuarioService } from 'src/app/Services/usuario.service';
+
 
 @Component({
   selector: 'app-producto',
@@ -14,96 +13,155 @@ import { NgModel } from '@angular/forms';
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent implements OnInit {
-  imagenes: any = []
+  imagenes:any=[]
   activatedRoute: any;
-  constructor(private productosService: ProductosService, private usuarioService: UsuarioService,
-    private route: ActivatedRoute, private router: Router) { }
+  imagenB64:any = null;
+  @Output() actualizarProducto = new EventEmitter<boolean>()
+  constructor(private productosService:ProductosService, private usuarioService:UsuarioService, private route: ActivatedRoute, private router: Router) { }
   @Input() categoriaActual: Categoria | any = {};
-  usuarioActual: any;
+  @Input() editandoProducto = false
+  @Input() productoActual:any
+  usuarioActual:any;
+
   ngOnInit(): void {
     this.formularioCrear.get('categoriaFormControl')?.setValue(this.categoriaActual.nombreCategoria)
     console.log(this.categoriaActual.nombreCategoria)
     this.obtenerUsuarioActual()
-
-    this.editarProducto()
+    console.log(this.editandoProducto)
+    if(this.editandoProducto){
+      this.enviarDatosProducto()
+    }
+    
   }
 
-  formularioCrear = new FormGroup({
+  formularioCrear = new FormGroup( {
     nombreFormControl: new FormControl("", [Validators.required]),
-    categoriaFormControl: new FormControl(this.categoriaActual?.nombreCategoria),
+    categoriaFormControl:new FormControl(this.categoriaActual?.nombreCategoria),
     costoFormControl: new FormControl("", [Validators.required]),
-    ubicacionFormControl: new FormControl("", Validators.required),
-    estadoFormControl: new FormControl('', [Validators.required]),
-    descripcionFormControl: new FormControl('', [Validators.required]),
-
+    ubicacionFormControl: new FormControl("",Validators.required),
+    estadoFormControl : new FormControl('', [Validators.required]),
+    descripcionFormControl: new FormControl('',[Validators.required]),
+    
   })
-  enviarFormulario() {
-
-    if (!this.formularioCrear.invalid) {
+  enviarFormulario(){
+    
+    if( !this.formularioCrear.invalid) {
 
       let producto = {
         nombre: this.formularioCrear.get('nombreFormControl')?.value,
         categoria: this.categoriaActual.idCategoria,
-        costo: this.formularioCrear.get('costoFormControl')?.value,
-        estado: this.formularioCrear.get('estadoFormControl')?.value,
-        descripcion: this.formularioCrear.get('descripcionFormControl')?.value,
+        costo:this.formularioCrear.get('costoFormControl')?.value,
+        estado:this.formularioCrear.get('estadoFormControl')?.value,
+        descripcion:this.formularioCrear.get('descripcionFormControl')?.value,
         ubicacion: this.formularioCrear.get('ubicacionFormControl')?.value,
         idUsuario: this.usuarioActual.idUsuario,
-
-
+        imagen: this.imagenB64
+        
+        
       }
 
-      this.productosService.crear(producto).subscribe((res: any) => {
+      this.productosService.crearProducto(producto).subscribe( (res:any) => {
         console.log(res)
-
+        this.actualizarProducto.emit(true)
       })
-    }
 
+    }
+   
+  }
+
+  async fileChangeEvent(event:any) {
+    if(event.length != 0){
+      this.imagenB64 = await this.toBase64(event[0])
+    }
 
   }
 
-  async convertirImagen(evento: any) {
 
+  //para varias imagenes TERMINAR DE HACER
+  async convertirImagen(evento:any){
+    
     console.log(evento);
     //creamos variable para convertir imagen a base64. Si no se puede pues se hace un for xD
-    for (let i = 0; i < evento.length; i++) {
+    for(let i=0;i<evento.length;i++){
 
-      let base64 = await this.toBase64(evento[i]);
-      this.imagenes.push({ name: evento[i].name, base64: base64 })
+      let  base64 = await this.toBase64(evento[i]);
+      this.imagenes.push( {name: evento[i].name, base64: base64})
     }
     //esto s ele debe de mandar al backend
-
-
+    
+    
   }
 
-  setActualizarImagenes(imagenes: any) {
+  setActualizarImagenes(imagenes:any) {
     // Esta función se dispara cuando se borra una imagen desde el componente de visualización de imagenes
     this.imagenes = imagenes
   }
 
-  toBase64 = (file: any) => new Promise((resolve, reject) => {
+  toBase64 = (file:any) => new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => resolve(reader.result);
     reader.onerror = error => reject(error);
-  });
+});
 
-  obtenerUsuarioActual() {
-    this.usuarioService.obtenerUsuarioActual().subscribe((res: any) => {
+  obtenerUsuarioActual(){
+    this.usuarioService.obtenerUsuarioActual().subscribe( (res:any) => {
       this.usuarioActual = res;
     })
   }
-
-  
+  /*
   editarProducto(): void {
     this.activatedRoute.params.subscribe(
       (      e: { [x: string]: any; })=>{
         let id=e['idProducto'];
       if(id){
-        this.productosService.getProducto(id).subscribe(
+        this.productosService.getProductoPorCategoria(id).subscribe(
           es=>this.editarProducto=es
         );
       }      }
     );
+  }*/
+
+  editarProductobeta(){
+
+    if(this.editandoProducto){
+      let producto = {
+        nombre: this.formularioCrear.get('nombreFormControl')?.value,
+        categoria: this.categoriaActual.idCategoria,
+        costo:this.formularioCrear.get('costoFormControl')?.value,
+        estado:this.formularioCrear.get('estadoFormControl')?.value,
+        descripcion:this.formularioCrear.get('descripcionFormControl')?.value,
+        ubicacion: this.formularioCrear.get('ubicacionFormControl')?.value,
+        idUsuario: this.usuarioActual.idUsuario,
+        imagen: this.imagenB64 || this.productoActual.imagen
+        
+        
+      }
+
+      this.productosService.editarProducto(this.productoActual.idProducto, producto).subscribe( (res:any) => {
+        console.log(res)
+        this.editandoProducto=false
+        this.actualizarProducto.emit(true)
+      })
     }
   }
+  
+  handleSubmit(){
+    if(this.editandoProducto){
+      this.editarProductobeta()
+    }else{
+      this.enviarFormulario()
+    }
+  }
+
+  enviarDatosProducto(){
+    this.formularioCrear.get('nombreFormControl')?.setValue(this.productoActual.nombre),
+    this.formularioCrear.get('costoFormControl')?.setValue(this.productoActual.costo),
+    this.formularioCrear.get('estadoFormControl')?.setValue(this.productoActual.estado),
+    this.formularioCrear.get('descripcionFormControl')?.setValue(this.productoActual.descripcion),
+    this.formularioCrear.get('ubicacionFormControl')?.setValue(this.productoActual.ubicacion)
+    console.log(this.formularioCrear.value)
+    console.log(this.productoActual)
+  }
+
+}
