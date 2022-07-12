140d41bc6c3612f21737824bd1b6bde37c9354d1
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-07-12 10:10:19 -0600

    Update categoria.component.ts

--- Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/categoria/categoria.component.ts
+++ Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/categoria/categoria.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Categoria } from 'src/app/interfaces/Categorias';
 import { CategoriasService } from 'src/app/Services/categorias.service';
 import { ProductosService } from 'src/app/Services/productos.service';
-
 
 @Component({
   selector: 'app-categoria',
@@ -11,6 +10,7 @@ import { ProductosService } from 'src/app/Services/productos.service';
   styleUrls: ['./categoria.component.css']
 })
 export class CategoriaComponent implements OnInit {
+  //agregada 
   listarProductos:any=[]
   categorias:Categoria[] = [
 
@@ -21,37 +21,56 @@ export class CategoriaComponent implements OnInit {
   ];
 
   @Input() categoriaActual:Categoria | undefined;
+  @Input() editandoProducto =false;
+  productoActual:any
+  
 
   constructor( private route: ActivatedRoute, private categoriaService:CategoriasService, private productosService:ProductosService) { }
 
   mostarFormulario = false;
 
   ngOnInit(): void {
-    //let idCategoria = parseInt(this.route.snapshot.paramMap.get('idCategoria') || "")
+    let idCategoria = parseInt(this.route.snapshot.paramMap.get('idCategoria') || "")
     //this.categoriaActual = this.categorias.filter(cat => cat.idCategoria == idCategoria)[0]
-    //this.obtenerCategoria(idCategoria)
-    this.obtenerProductos()
+    this.obtenerCategoria(idCategoria)
+    
 
   }
 
 
-  obtenerProductos(){
-    this.productosService.getProductos().subscribe(data =>{
-      this.listarProductos = data;
-      console.log(this.listarProductos);
+  obtenerProductosPorId() {
+    console.log(`Obteniendo los productos para la categoría: `)
+    console.log(this.categoriaActual)
+    this.mostarFormulario=false
+    this.productosService.getProductoPorCategoria(this.categoriaActual?.idCategoria).subscribe((res:any)=>{
+      this.listarProductos = res;
+      this.editandoProducto=false
+      
+      console.log(res)
+    }, (err:any)=>{
+      console.log(this.listarProductos)
     })
+    
+  }
+
+  editarProducto(objProducto:any){
+    this.mostarFormulario=true
+    this.editandoProducto = true
+    this.productoActual = objProducto
+    
   }
 
   setMostrarFormulario() {
     this.mostarFormulario = !this.mostarFormulario;
+    this.editandoProducto=false
   }
 
-  /*obtenerCategoria(idCategoria:number) {
+  obtenerCategoria(idCategoria:number) {
     this.categoriaService.getCategoria(idCategoria).subscribe( (res:any) => {
       this.categoriaActual = res;
+      this.obtenerProductosPorId()
       console.log(res)
     })
   }
-  */
 
 }
