e27287a2ef86e603288e8954623be0c335914f3b
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-07-12 10:08:26 -0600

    Update card-producto.component.ts

--- Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/card-producto/card-producto.component.ts
+++ Reserva Proyecto/Frontend/KAPEKSHOP/src/app/Components/card-producto/card-producto.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Categoria } from 'src/app/interfaces/Categorias';
+import { ConfigModal } from 'src/app/interfaces/config-modal';
+import { ProductosService } from 'src/app/Services/productos.service';
+import { UsuarioService } from 'src/app/Services/usuario.service';
+import { ModalConfirmacionComponent } from '../modal-confirmacion/modal-confirmacion.component';
 
 @Component({
   selector: 'app-card-producto',
@@ -9,14 +15,59 @@ export class CardProductoComponent implements OnInit {
 
   @Input() producto:any;
   @Input() listarProducto:any
+  @Input() categoria:Categoria | undefined;
+  @Output() onEvento = new EventEmitter<boolean>();
+  @Output() onEditarProducto = new EventEmitter<any>();
+ 
   estrellas:any
+  usuarioActual: any
 
-  constructor() { }
+  constructor(private productoService: ProductosService, private usuarioService:UsuarioService, private modalService:NgbModal) { }
 
   ngOnInit(): void {
-    console.log(this.producto)
     console.log(this.listarProducto)
-    this.estrellas = new Array(this.producto.valoracion).fill(1)
+    
+    //this.estrellas = new Array(this.producto.valoracion).fill(1)
+  }
+
+  mostrarProducto(){
+    this.productoService.actualizarContador(this.listarProducto.idProducto).subscribe( res => {
+      console.log(res)
+    }, err => {
+      console.log(err)
+    })
+
+    // Mostrar el componente producto
+  }
+
+  
+  /*AGREGAR FUNCION SI EL USUARIO ACTUAL NO ES QUIEN AGREGÓ UN PRODUCTO ENTONCES QUE NO PUEDA ELIMINAR O EDITAR*/
+  obtenerUsuarioActual() {
+    this.usuarioService.obtenerUsuarioActual().subscribe( (res:any) => {
+      this.usuarioActual = res;
+    })
+  }
+
+  modalEliminar(){
+    let modalRef:NgbModalRef;
+    modalRef = this.modalService.open(ModalConfirmacionComponent)
+    let configuracion:ConfigModal = {
+      titulo1: '¿Está seguro de eliminar el producto?',
+      titulo2:'Se eliminará el producto',
+      parametros:{eliminarProducto:true, idProducto:this.listarProducto.idProducto}
+    }
+   
+    modalRef.componentInstance.mensaje = configuracion
+    
+    modalRef.componentInstance.onEvento.subscribe((res:any)=>[
+      this.onEvento.emit(true)
+    ])
+  }
+
+
+  editarProducto(){
+    
+    this.onEditarProducto.emit(this.listarProducto)
   }
 
 }
