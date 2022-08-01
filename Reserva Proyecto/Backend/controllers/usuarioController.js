d852339209435587db154bc51cea755228cedcca
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-08-01 14:48:27 -0600

    Update usuarioController.js

--- Reserva Proyecto/Backend/controllers/usuarioController.js
+++ Reserva Proyecto/Backend/controllers/usuarioController.js
@@ -183,12 +183,6 @@ exports.obtenerDenunciasP = (req, res) => {
     })
 }
 
-exports.obtenerUsuarioPorId = (req,res)=>{
-    Usuario.obtenerUsuarioPorId(req.params.idUsuario, (err,data)=>{
-        if(err) return res.status(500).send({msj:err})
-        return res.status(200).json(data)
-    })
-}
 
 exports.obtenerUsuarioPorId = (req,res)=>{
     Usuario.obtenerUsuarioPorId(req.params.idUsuario, (err,data)=>{
